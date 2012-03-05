f9af8b557d481c71a6f20071a1ebd44aa3678265
Michael Hodgson <michaelwhodgson@gmail.com>
2012-03-05 00:41:59 +0000

    added multithreading :D

--- Classifier.py
+++ Classifier.py
@@ -14,6 +14,7 @@ import cPickle as pickle
 from defaultconfig import default
 from randWindowExtractor import randWindowExtractor
 
+import multiprocessing
 
 
 cellsize = 6
@@ -21,6 +22,49 @@ cellsize = 6
 window_cell_shape = default['window_cell_shape']
 
 window_pixel_shape = (window_cell_shape[0] * cellsize, window_cell_shape[1] * cellsize)
+
+
+# some code from http://broadcast.oreilly.com/2009/04/pymotw-multiprocessing-part-2.html
+class WindowWorker(multiprocessing.Process):
+    
+    def __init__(self, task_queue, result_queue, img, svc, scaler, normcells):
+        multiprocessing.Process.__init__(self)
+        self.task_queue = task_queue
+        self.result_queue = result_queue
+        
+        
+        self.img = img
+        self.svc = svc
+        self.scaler = scaler
+        self.normcells = normcells
+
+    def run(self):
+        proc_name = self.name
+        while True:
+            next_task = self.task_queue.get()
+            if next_task is None:
+                # Poison pill means we should exit
+                print '%s: Exiting' % proc_name
+                self.task_queue.task_done()
+                break
+            #print '%s: %s' % (proc_name, next_task)
+            answer = self.process_window(next_task[0], next_task[1])
+            if answer is not None:
+                self.result_queue.put(answer)
+                print answer
+            self.task_queue.task_done()
+        return
+        
+    def process_window(self, x, y):
+        window = self.normcells[x:x + window_cell_shape[0], y:y + window_cell_shape[1]]
+        window = window.flatten()
+        window_s = self.scaler.transform(window)
+        prediction = self.svc.predict(window_s)               
+    
+        if prediction >= 0.5:
+            return (x * cellsize, y * cellsize, prediction)
+        else:
+            return None
 
 def detect_humans(img, svc, scaler, signed=False, debug=True, extract_humans_to=None):
     window_hits = np.zeros([img.shape[0] // cellsize, img.shape[1] // cellsize])
@@ -33,6 +77,9 @@ def detect_humans(img, svc, scaler, signed=False, debug=True, extract_humans_to=
     
     #detected_humans.append((0, 0, 1.0))
     
+    pool_size = multiprocessing.cpu_count() * 2
+    pool = multiprocessing.Pool(processes=pool_size)
+    
     for x in range(0, img.shape[0] // cellsize):
         if debug:
             print x, "of", img.shape[0] // cellsize
@@ -43,6 +90,7 @@ def detect_humans(img, svc, scaler, signed=False, debug=True, extract_humans_to=
             if (y + window_cell_shape[1]) > img.shape[1] // cellsize:
                 break
             
+        
             window = normcells[x:x + window_cell_shape[0], y:y + window_cell_shape[1]]
             window = window.flatten()
             window_s = scaler.transform(window)
@@ -58,6 +106,63 @@ def detect_humans(img, svc, scaler, signed=False, debug=True, extract_humans_to=
             #print "Prediction at", x // window_move_stride, y // window_move_stride, "of", window_hits.shape, "is", prediction
             window_hits[x, y] = prediction
             
+    
+    return window_hits, detected_humans
+
+def detect_humans_multi(img, svc, scaler, signed=False, debug=True, extract_humans_to=None):
+    window_hits = np.zeros([img.shape[0] // cellsize, img.shape[1] // cellsize])
+    detected_humans = []
+    
+    window_cell_shape = (window_pixel_shape[0] // cellsize, window_pixel_shape[1] // cellsize)
+    
+    
+    orients, normcells = HOG.HOG(img, signed, cellsize=cellsize)
+    
+    #detected_humans.append((0, 0, 1.0))
+    
+    pool_size = multiprocessing.cpu_count() * 2
+    pool = multiprocessing.Pool(processes=pool_size)
+    
+    window_locations = []
+    
+    for x in range(0, img.shape[0] // cellsize):
+        if debug:
+            print x, "of", img.shape[0] // cellsize
+
+        if (x + window_cell_shape[0]) > img.shape[0] // cellsize:
+            break
+        for y in range(0, img.shape[1] // cellsize):
+            if (y + window_cell_shape[1]) > img.shape[1] // cellsize:
+                break
+            
+            window_locations.append((x, y))
+    
+    # some code from http://broadcast.oreilly.com/2009/04/pymotw-multiprocessing-part-2.html
+    tasks = multiprocessing.JoinableQueue()
+    results = multiprocessing.Queue()
+    
+    num_consumers = multiprocessing.cpu_count() * 2
+    print 'Creating %d consumers' % num_consumers
+    consumers = [ WindowWorker(tasks, results, img, svc, scaler, normcells) for i in xrange(num_consumers) ]
+    
+    for w in consumers:
+        w.start()
+
+    for window_loc in window_locations:
+        tasks.put(window_loc)
+    
+    # Add a poison pill for each consumer
+    for i in xrange(num_consumers):
+        tasks.put(None)
+    
+    print "Joining"
+    tasks.join()
+    
+    detected_humans = []
+    while not results.empty():
+        detected_humans.append(results.get())
+    
+    print "Num results found:", len(detected_humans)
     
     return window_hits, detected_humans
 
@@ -104,7 +209,7 @@ def run_prog():
         print "Image shape", img.shape
         
 
-        window_hits, detected_humans = detect_humans(img, svc, scaler, options.signed, debug=True)
+        window_hits, detected_humans = detect_humans_multi(img, svc, scaler, options.signed, debug=True)
         humans_scale.append((scale, detected_humans))
     
     
